2622431718aefeac585b8f815f3ecfff9f3e6073
ga45qeg <m.kuehne@tum.de>
2018-07-23 23:11:31 +0200

    Added histogram and compressed-histogram extraction to extract_tensorboard_data Note: reading untested

--- extract_tensorboard_data.py
+++ extract_tensorboard_data.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 from sys import argv
 from collections import namedtuple
+import json
 
 
 def get_from_commandline_args(count, args_string=None):
@@ -26,16 +27,15 @@ def get_prefix_and_model_name():
 
 def load_tensorflow_log(path):
 
-    # # Loading too much data is slow...
-    # tf_size_guidance = {
-    #     'compressedHistograms': 10,
-    #     'images': 0,
-    #     'scalars': 100,
-    #     'histograms': 1
-    # }
+    tf_size_guidance = {
+        'compressed_histograms': 10000,
+        'images': 0,
+        'scalars': 10000,
+        'histograms': 10000
+    }
 
-    # event_acc = EventAccumulator(path, tf_size_guidance)
-    event_acc = EventAccumulator(path)
+    event_acc = EventAccumulator(path, tf_size_guidance)
+    # event_acc = EventAccumulator(path)
     event_acc.Reload()
     return event_acc
 
@@ -51,32 +51,92 @@ def get_scalars(event_acc):
 
 
 def get_histograms(event_acc):
-    # to be plotted with plt.plot(h.buckets, h.count) ; plt.xlim(-1,1)
-    Histogram = namedtuple('Histogram', ('name', 'buckets', 'count'))
-    return [Histogram(s, np.asarray(event_acc.Histograms(s))[0][2][5], np.asarray(event_acc.Histograms(s))[0][2][6])
-            for s in event_acc.Tags()['histograms']]
+    # to be plotted with plt.plot(h.bucket_low_limit, h.bucket_filling) ; plt.xlim(-1,1) // how handle low limit?
+    Histogram = namedtuple('Histogram', ('name', 'steps'))
+    HistogramStep = namedtuple('HistogramStep', ('name', 'step', 'bucket_low_limit', 'bucket_filling'))
+
+    hist_list = []
+    for h in event_acc.Tags()['histograms']:
+        step_list = [HistogramStep(h, hist_step.step, hist_step[2][5], hist_step[2][6])
+                     for hist_step in event_acc.Histograms(h)]
+
+        hist_list.append(Histogram(h, step_list))
+
+    return hist_list
 
 
-def untested_plot(event_acc):
-    training_accuracies = event_acc.Scalars('training-accuracy')
-    validation_accuracies = event_acc.Scalars('validation_accuracy')
+def get_compressed_histograms(event_acc):
+    # Normal CDF for std_devs: (-Inf, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, Inf)
+    # naturally gives bands around median of width 1 std dev, 2 std dev, 3 std dev,
+    # and then the long tail.
+    # NORMAL_HISTOGRAM_BPS = (0, 668, 1587, 3085, 5000, 6915, 8413, 9332, 10000)
 
-    steps = 10
-    x = np.arange(steps)
-    y = np.zeros([steps, 2])
+    CompressedHistogram = namedtuple('CompressedHistogram', ('name', 'steps',
+                                                             'Infm', 'stdm3', 'stdm2', 'stdm1',
+                                                             'median',
+                                                             'stdp1', 'stdp2', 'stdp3', 'Infp'))
 
-    for i in range(steps):
-        y[i, 0] = training_accuracies[i][2]  # value
-        y[i, 1] = validation_accuracies[i][2]
+    chist_list = []
+    for ch_name in event_acc.Tags()['distributions']:
+        name = ch_name
+        ch = event_acc.CompressedHistograms(ch_name)
+        steps = []
+        Infm = []
+        stdm3 = []
+        stdm2 = []
+        stdm1 = []
+        median = []
+        stdp1 = []
+        stdp2 = []
+        stdp3 = []
+        Infp = []
+        for ch_step in ch:
+            steps.append(ch_step.step)
+            Infm.append(ch_step.compressed_histogram_values[0][1])
+            stdm3.append(ch_step.compressed_histogram_values[1][1])
+            stdm2.append(ch_step.compressed_histogram_values[2][1])
+            stdm1.append(ch_step.compressed_histogram_values[3][1])
+            median.append(ch_step.compressed_histogram_values[4][1])
+            stdp1.append(ch_step.compressed_histogram_values[5][1])
+            stdp2.append(ch_step.compressed_histogram_values[6][1])
+            stdp3.append(ch_step.compressed_histogram_values[7][1])
+            Infp.append(ch_step.compressed_histogram_values[8][1])
+        chist_list.append(CompressedHistogram(name, steps,
+                                              Infm, stdm3, stdm2, stdm1,
+                                              median,
+                                              stdp1, stdp2, stdm3, Infp))
 
-    plt.plot(x, y[:, 0], label='training accuracy')
-    plt.plot(x, y[:, 1], label='validation accuracy')
+    return chist_list
 
-    plt.xlabel("Steps")
-    plt.ylabel("Accuracy")
-    plt.title("Training Progress")
-    plt.legend(loc='upper right', frameon=True)
-    plt.show()
+
+def save_to_dir(directory, scalars, histograms, compressed_histograms):
+    if not directory.endswith("/"):
+        directory += "/"
+
+    names = []
+
+    # store scalars
+    for s in scalars:
+        npyname = "scalar_" + s.name + '_' + model_name + ".npy"
+        print("Storing scalar '%s' at %s …" % (s.name, npyname))
+        np.save(directory + npyname, s.values)
+        names.append(directory + npyname)
+
+    for h in histograms:
+        pklname = "hist_" + h.name + "_" + model_name + ".json"
+        print("Storing histogram '%s' at %s …" % (h.name, pklname))
+        with open(directory + pklname, 'w') as f:
+            json.dump(h, f)
+        names.append(directory + pklname)
+
+    for ch in compressed_histograms:
+        pklname = "comp_hist_" + ch.name + "_" + model_name + ".json"
+        print("Storing compressed histogram '%s' at %s …" % (ch.name, pklname))
+        with open(directory + pklname, 'w') as f:
+            json.dump(ch, f)
+        names.append(directory + pklname)
+
+    return names
 
 
 if __name__ == '__main__':
@@ -86,25 +146,33 @@ if __name__ == '__main__':
     checkpoint_dir = r"/home/matthias-k/GraphLSTM_data/%s" % prefix
     checkpoint_dir += r"/%s" % model_name
     tensorboard_dir = checkpoint_dir + r"/tensorboard"
+    validation_dir = tensorboard_dir + r"/validation"
 
-    log_file = tensorboard_dir
-
-    print("Loading tensorboard data … (this may take a long time)")
-    eva = load_tensorflow_log(log_file)
+    print("Loading training tensorboard data … (this may take a long time)")
+    eva = load_tensorflow_log(tensorboard_dir)
     print("Done. Log contents:")
     print_log_contents(eva)
 
-    print("Converting data …")
+    print("\nConverting data …")
     scalars = get_scalars(eva)
     histograms = get_histograms(eva)
+    compressed_histograms = get_compressed_histograms(eva)
 
-    # store scalars
-    for s in scalars:
-        npyname = s.name + '_' + model_name
-        print("Storing '%s' at %s …" % (s.name, npyname))
-        np.save(checkpoint_dir + "/" + npyname, s.values)
+    path_list = save_to_dir(tensorboard_dir, scalars, histograms, compressed_histograms)
 
-    # store histograms
-    # TODO
+    print("\nLoading validation tensorboard data … (this may take a long time, but shorter than for training)")
+    eva = load_tensorflow_log(validation_dir)
+    print("Done. Log contents:")
+    print_log_contents(eva)
 
-    print("Done, exiting.")
+    print("\nConverting data …")
+    scalars = get_scalars(eva)
+    histograms = get_histograms(eva)
+    compressed_histograms = get_compressed_histograms(eva)
+
+    path_list.extend(save_to_dir(validation_dir, scalars, histograms, compressed_histograms))
+
+    print("Done.")
+    print("The following files have been created:")
+    for name in path_list:
+        print(name)
